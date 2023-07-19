510a867819cff22480866aec524f3d52fb6cbf68
ice-lfernandes <ice_lfernandes@uolinc.com>
2023-07-19 08:56:44 -0300

    melhoria na tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginViewComponent } from './view/login-view/login-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginViewComponent } from './view/login-view/login-view.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
