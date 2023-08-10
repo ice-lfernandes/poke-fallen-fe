5be56d73b76765c59a0fb8f35ecf0c6d05f7fef1
Bianca <ice_bpinheiro@uolinc.com>
2023-08-09 23:04:39 -0300

    feat/Bia: edita navbar e introdução

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { FirstLoginViewComponent } from './view/first-login-view/first-login-vie
 import { HomeViewComponent } from './view/home-view/home-view.component';
 import { PlayerDetailsViewComponent } from './view/internal/player-details-view/player-details-view.component';
 import { PlayerGameSaveViewComponent } from './view/internal/player-game-save-view/player-game-save-view.component';
-import { AuthGaurdService } from './service/authentication/auth/authguard.service';
+import { AuthGuardService } from './service/authentication/auth/authguard.service';
 import { AdminPlayersViewComponent } from './view/internal/admin-players-view/admin-players-view.component';
 
 const routes: Routes = [
@@ -14,9 +14,9 @@ const routes: Routes = [
   { path: 'home', component: HomeViewComponent },
   { path: 'login', component: LoginViewComponent },
   { path: 'first-login', component: FirstLoginViewComponent },
-  { path: 'player-details', component: PlayerDetailsViewComponent, canActivate: [AuthGaurdService] },
-  { path: 'player-game-save', component: PlayerGameSaveViewComponent, canActivate: [AuthGaurdService] },
-  { path: 'players-details', component: AdminPlayersViewComponent, canActivate: [AuthGaurdService] }
+  { path: 'player-details', component: PlayerDetailsViewComponent, canActivate: [AuthGuardService] },
+  { path: 'player-game-save', component: PlayerGameSaveViewComponent, canActivate: [AuthGuardService] },
+  { path: 'players-details', component: AdminPlayersViewComponent, canActivate: [AuthGuardService] }
 ];
 
 @NgModule({
