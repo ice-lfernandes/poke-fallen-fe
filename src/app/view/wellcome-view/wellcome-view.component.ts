9df0422b8d53e76fa4c1593d399aa28e3c86902e
Bianca <ice_bpinheiro@uolinc.com>
2023-08-09 23:48:23 -0300

    feat/Bia: começa edição sinopse

---
+++ src/app/view/wellcome-view/wellcome-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wellcome-view',
+  templateUrl: './wellcome-view.component.html',
+  styleUrls: ['./wellcome-view.component.css']
+})
+export class WellcomeViewComponent {
+
+}
