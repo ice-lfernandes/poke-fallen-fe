546c94b0418cd850ea7207eb693c2fb0aaec8af0
ice-lfernandes <lucas.rj.fernandes@gmail.com>
2024-02-20 14:20:55 -0300

    criando modal para adição de blocos

--- src/app/view/internal/admin/manager-block-rewards-view/manager-block-rewards-view.component.ts
+++ src/app/view/internal/admin/manager-block-rewards-view/manager-block-rewards-view.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { faSearch, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { faPlus, faSearch, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { OperatorFunction, Observable, debounceTime, distinctUntilChanged, map } from 'rxjs';
 
 import { BlockPreAwardItemService } from 'src/app/service/integration/block-pre-award-item.service';
 import { BlockPreAwardItem } from 'src/app/service/integration/model/commons/block-pre-award-item';
+import { ItemImage } from 'src/app/service/integration/model/commons/item-image';
+import { PokemonImage } from 'src/app/service/integration/model/commons/pokemon-image';
 import { PreAwardItem } from 'src/app/service/integration/model/commons/pre-award-item';
 import { ImageComposition } from 'src/app/shared/images/image-composition';
 
@@ -19,8 +22,18 @@ export class ManagerBlockRewardsViewComponent implements OnInit {
   // Icons
   faTrash = faTrash
   faSearch = faSearch
+  faPlus = faPlus
 
   blocks: BlockPreAwardItem[] = []
+  pokemonsImage: PokemonImage[] = []
+  itemsImage: ItemImage[] = []
+  typeItemChoose: string = 'pokemon'
+  itemChoose: any;
+  pokemonChoose: any;
+  pokemonSelected: PokemonImage | undefined
+  itemSelected: ItemImage | undefined
+  newBlock: BlockPreAwardItem = new BlockPreAwardItem()
+  closeResult = '';
 
   // Filters
   blockNameSearchFilter: string | undefined
@@ -112,5 +125,65 @@ export class ManagerBlockRewardsViewComponent implements OnInit {
       )
     })
     return images
+  }
+
+  newItem(contentNewItem: any) {
+    this.modalService.open(contentNewItem, { size: 'lg' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      },
+    );
+  }
+
+
+  searchPokemons: OperatorFunction<string, readonly String[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map((term) =>
+        term.length < 2 ? [] : this.pokemonsImage.map(p => p.name).filter((v) => v.indexOf(term.toUpperCase()) > -1).slice(0, 10),
+      )
+    );
+
+  searchItems: OperatorFunction<string, readonly String[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map((term) =>
+        term.length < 2 ? [] : this.itemsImage.map(i => i.name).filter((v) => v.indexOf(term.toUpperCase()) > -1).slice(0, 10),
+      )
+    );
+
+    updateModel() {
+      if (this.typeItemChoose == 'pokemon') {
+        this.pokemonSelected = this.pokemonsImage.find(p => p.name == this.itemChoose)
+  
+        
+  
+      } else if (this,this.typeItemChoose == 'item') {
+        this.itemSelected = this.itemsImage.find(i => i.name == this.itemChoose)
+  
+        
+  
+      } else {
+        this.pokemonSelected = this.pokemonsImage.find(p => p.name == this.pokemonChoose)
+  
+        
+      }
+    }
+  
+
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
