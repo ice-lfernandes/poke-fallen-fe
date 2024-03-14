24416ee3452d2abe07668022e96355b0bf2ba3ca
ice-lfernandes <lucas.rj.fernandes@gmail.com>
2024-03-14 12:33:35 -0300

    ajuste e url de qa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080',
-    siteUrl: 'http://localhost:4200'
-  };
+  production: false,
+  apiUrl: 'http://poke-fallen-qa-2-env.us-east-1.elasticbeanstalk.com/',
+  siteUrl: 'http://localhost:4200'
+};
